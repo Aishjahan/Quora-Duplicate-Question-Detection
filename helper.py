b4d10d5c6cdce582d14438705aee27e3fe26be7e
Aishjahan <aishjahankhan7376@gmail.com>
2024-12-14 18:35:48 +0530

    Added stopwards.pkl file

--- helper.py
+++ helper.py
@@ -4,7 +4,9 @@ import distance
 from fuzzywuzzy import fuzz
 import pickle
 import numpy as np
+import nltk
 from nltk.corpus import stopwords
+nltk.download('stopwords')
 
 cv = pickle.load(open('cv.pkl','rb'))
 
